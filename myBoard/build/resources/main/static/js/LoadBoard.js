63bf028aacf57374799eb471dc52fabc2cce2006
DESKTOP-ANVN13Q\gabur <gabury1@gmail.com>
2022-07-09 02:11:32 +0900

    첫 커밋 + 빌드

--- myBoard/build/resources/main/static/js/LoadBoard.js
+++ myBoard/build/resources/main/static/js/LoadBoard.js
@@ -57,3 +57,70 @@ function pageChange(pPage)
 }
 
 
+function success(boardlist)
+{
+                 let html = '<tr> <th width="10%">no</th><th width="50%">content</th><th width="40%">name</th></tr>';
+
+                if( boardlist.data.length == 0 ){ // 검색 결과가 존재하지 않으면
+                          html +=
+                                '<tr>'+
+                                        '<td colspan="5">No result</td> '+
+                                 '</tr>';
+                }else{
+                        for( let i = 0 ; i<boardlist.data.length ; i++ ){
+                            html +=
+                                    '<tr>'+
+                                            '<td>'+boardlist.data[i].bno+'</td> '+
+                                            '<td>'+boardlist.data[i].bcontent+'</td> '+
+                                            '<td>'+boardlist.data[i].whoqwe+'</td>'+
+                                     '</tr>';
+                        }
+                 }
+//////////////////////////////////////////////////////////////////////////////////////// 페이징 버튼 생성 코드 ///////////////////////////////////////////////////////////////////////
+                 let pagehtml = "";
+                 ////////////////////////////////////////////  이전 버튼 ////////////////////////////////////////////////
+                 if( page == 0 ){   // 현재 페이지가 첫페이지 이면
+                        pagehtml +=
+                         '<li class="page-item"> '+
+                                     '<button class="page-link" onclick="board_list('+ (page)  +')"> 이전 </button>'+  // 검색 없음
+                          '</li>';
+                 }else{  // 현재 페이지가 첫페이지가 아니면
+                     pagehtml +=
+                        '<li class="page-item"> '+
+                                    '<button class="page-link" onclick="board_list('+ (page-1)  +')"> 이전 </button>'+  // 검색 없음
+                         '</li>';
+                  }
+                 ////////////////////////////////////////////  ////////////////////////////////// ////////////////////////////////////////////////
+                ////////////////////////////////////////// 가운데에 들어가는 페이징 버튼수 //////////////////////////////////////////
+                 for( let i = boardlist.startbtn ; i<=boardlist.endhtn ; i++ ){
+                    pagehtml +=
+                          '<li class="page-item"> '+
+                            '<button class="page-link" onclick="board_list('+(i-1)+')"> '+i+' </button>'+  // 검색 없음
+                          '</li>';
+                 }
+                ///////////////////////////////////////// ///////////////////////////////////////  //////////////////////////////////////////
+                ////////////////////////////////////////////  다음 버튼 ////////////////////////////////////////////////
+                if( page == boardlist.totalpages -1 ){ // 현재 페이지가 마지막 페이지이면
+                     pagehtml +=
+                            '<li class="page-item"> '+
+                                        '<button class="page-link" onclick="board_list('+ (page)  +')"> 다음 </button>'+  // 검색 없음
+                             '</li>';
+                }else{ // 아니면
+                     pagehtml +=
+                        '<li class="page-item"> '+
+                                    '<button class="page-link" onclick="board_list('+ (page+1)  +')"> 다음 </button>'+  // 검색 없음
+                         '</li>';
+                }
+
+                ////////////////////////////////////////////  ////////////// ////////////////////////////////////////////////
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                $("#boardtable").html( html ); // 테이블에 html  넣기
+                $("#pagebtnbox").html( pagehtml); // 페이징버튼 html 넣기
+
+
+}
+
+
+
+
